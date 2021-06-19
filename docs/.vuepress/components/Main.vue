fe2db3cda1ae188be4139ac1f6a5f1d9df855624
G1 <g1.jeon@kakao.com>
2021-06-20 00:04:19 +0900

    add: Font Style

--- docs/.vuepress/components/Main.vue
+++ docs/.vuepress/components/Main.vue
@@ -25,6 +25,17 @@
 </script>
 
 <style>
+@font-face {
+    font-family: 'IBMPlexSansKR-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+html, body {
+  font-family: "IBMPlexSansKR-Regular"
+}
+
 h1 {
   font-size: 5rem;
 }
