98a28303a67bf5665034fa8c7db4a135b1bd34b0
Tommy <g1.tommy.jeon@gmail.com>
2022-01-22 14:14:38 +0900

    feat: Font Style Changed

--- docs/.vuepress/components/Main.vue
+++ docs/.vuepress/components/Main.vue
@@ -26,14 +26,29 @@
 
 <style>
 @font-face {
-    font-family: 'IBMPlexSansKR-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
-    font-weight: normal;
+    font-family: '11STREETGothic';
+    src: url('../fonts/11STREET_Gothic_regular.otf') format('opentype');
+    font-weight: 400;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: '11STREETGothic';
+    src: url('../fonts/11STREET_Gothic_light.otf') format('opentype');
+    font-weight: 300;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: '11STREETGothic';
+    src: url('../fonts/11STREET_Gothic_bold.otf') format('opentype');
+    font-weight: 700;
     font-style: normal;
 }
 
 html, body {
-  font-family: "IBMPlexSansKR-Regular"
+  font-family: "11STREETGothic";
+  font-style: 400;
 }
 
 h1 {
